5f6619c1d308dd6deb44b0c469d6a5f1e1244309
Roberto Guedes <robertoguedes@edu.univali.br>
2024-09-18 22:13:32 -0300

    feat(decorators): logger e timer

--- etl.py
+++ etl.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import os
 import glob
+from log import log_decorator
+from timer import timer_decorator
 
 # Função de extract que lê e consolida
+@timer_decorator
+@log_decorator
 def extrair_dados(pasta: str) -> pd.DataFrame:
     arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
@@ -11,12 +15,16 @@ def extrair_dados(pasta: str) -> pd.DataFrame:
 
 
 # Função que transforma o df adicionando o total de cada venda
+@timer_decorator
+@log_decorator
 def transformar_dados(df: pd.DataFrame) -> pd.DataFrame:
     df['Total'] = df['Quantidade'] * df['Venda']
     return df
 
 
 # Função que salva em csv ou parquet
+@timer_decorator
+@log_decorator
 def carregar_dados(df: pd.DataFrame, formatos: list[str]):
     for formato in formatos:
         if formato == 'csv':
@@ -26,6 +34,8 @@ def carregar_dados(df: pd.DataFrame, formatos: list[str]):
 
 
 # Função da pipeline que chama a ETL
+@timer_decorator
+@log_decorator
 def pipeline(pasta_entrada: str, formato_saida: list[str]):
     dados = extrair_dados(pasta_entrada)
     dados_transformados = transformar_dados(dados)
